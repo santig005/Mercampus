87c99194f99fed3830a8d580a1921078bcbf367b
Marco <mgomezp17@eafit.edu.co>
2024-09-08 13:41:37 -0500

    init project

--- src/app/api/product/route.js
+++
@@ -1,7 +0,0 @@
-import { connectDB } from '@/connectDB';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  connectDB();
-  return NextResponse.json({ products: { name: 'Product 1', price: 100 } });
-}
