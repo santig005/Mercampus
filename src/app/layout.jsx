75eade7fdfbbf05c6e449ec2dec5326bae26d085
Marco <mgomezp17@eafit.edu.co>
2024-09-08 01:44:05 -0500

    feat & fix: add prettier & fix connectDB

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,25 @@
-import { Inter } from "next/font/google";
-import {Providers} from './Providers';
+import { Montserrat } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import React from 'react';
+import '../../public/main.css';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+  weight: ['400', '500', '600', '700'],
+});
 
 export const metadata = {
-  title: "Mercampus",
+  title: 'Mercampus',
   description:
-    "Calma tus antojos con Mercampus, un marketplace para universitarios donde podrás comprar y vender tus artículos.",
+    'Calma tus antojos con Mercampus, un marketplace para universitarios donde podrás comprar y vender tus artículos.',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <body className={inter.className}>
-        <Providers>
-          {children}
-          </Providers>
-        </body>
+    <html lang='es' className={montserrat.className}>
+      <body>{children}</body>
     </html>
   );
 }
