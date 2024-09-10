3db1707bb13f645205a1470e38b24cbdc98a6384
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-09-10 14:47:40 -0500

    feat: add bcrypt

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,4 @@
 import { handlers } from "@/lib/auth" // Referring to the auth.ts we just created
+import { connectDB } from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
 export const { GET, POST } = handlers
