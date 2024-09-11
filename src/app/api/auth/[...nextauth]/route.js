048f574e62314f986bdd2260f541a6a5c7fea9d0
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-09-11 14:14:52 -0500

    fix: fix merge

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 import { handlers } from "@/utils/lib/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
-=======
-import { handlers } from "@/lib/auth" // Referring to the auth.ts we just created
-import { connectDB } from "@/lib/mongodb";
-import bcrypt from "bcryptjs";
-export const { GET, POST } = handlers
->>>>>>> 56f635a2d2b1acb2f083b20eea6d87d056f8115a
