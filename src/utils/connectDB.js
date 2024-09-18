04a190de72af093d044ae27f7da00c701ee67746
VictorJVA <vjvilladia@eafit.edu.co>
2024-09-18 17:22:06 -0500

    fix: handle errors

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,20 +1,30 @@
 import { connect, connection } from 'mongoose';
-
+ 
 const connected = {
   isConnected: false,
 };
-
+ 
 export async function connectDB() {
   if (connected.isConnected) return;
-  const db = await connect(process.env.MONGO_URI);
-  connected.isConnected = db.connections[0].readyState;
-  console.log(db.connection.db.databaseName);
+ 
+  try {
+    const db = await connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      serverSelectionTimeoutMS: 5000, // Aumenta el tiempo de espera de la conexión
+    });
+ 
+    connected.isConnected = db.connections[0].readyState;
+    console.log(db.connection.db.databaseName);
+  } catch (error) {
+    console.error('Error al conectar con la base de datos:', error);
+    process.exit(1); // Salir del proceso si no se puede conectar con la base de datos
+  }
 }
-
+ 
 connection.on('connected', () => {
   console.log('Database connected');
 });
-
-connection.on('error', err => {
-  console.log('Database connection error', err);
+ 
+connection.on('error', (err) => {
+  console.error('Database connection error:', err);
 });
