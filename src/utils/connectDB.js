09a4b4142575819b2b47bbda576816849df8c7c9
santig005 <sajhdg30@gmail.com>
2024-09-18 19:41:04 -0500

    feat: database connection with more details

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -6,15 +6,25 @@ const connected = {
 
 export async function connectDB() {
   if (connected.isConnected) return;
-  const db = await connect(process.env.MONGO_URI);
-  connected.isConnected = db.connections[0].readyState;
-  console.log(db.connection.db.databaseName);
+
+  try {
+    const db = await connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      serverSelectionTimeoutMS: 5000, // Aumenta el tiempo de espera de la conexión
+    });
+
+    connected.isConnected = db.connections[0].readyState;
+    console.log(db.connection.db.databaseName);
+  } catch (error) {
+    console.error('Error al conectar con la base de datos:', error);
+    process.exit(1); // Salir del proceso si no se puede conectar con la base de datos
+  }
 }
 
 connection.on('connected', () => {
   console.log('Database connected');
 });
 
-connection.on('error', err => {
-  console.log('Database connection error', err);
+connection.on('error', (err) => {
+  console.error('Database connection error:', err);
 });
