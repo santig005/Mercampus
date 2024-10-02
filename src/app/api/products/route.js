9762e08f27eee55ac84dc6cbcdb6c96e3883e1b9
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-10-02 11:52:07 -0500

    fix: merge develop

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { connectDB } from "@/utils/connectDB";
 import { NextResponse } from "next/server";
 import { Product } from "@/utils/models/productSchema";
-=======
-import { connectDB } from '@/utils/connectDB';
-import { NextResponse } from 'next/server';
-import { Product } from '@/utils/models/productSchema';
->>>>>>> 56f635a2d2b1acb2f083b20eea6d87d056f8115a
 
 export async function GET() {
   connectDB();
