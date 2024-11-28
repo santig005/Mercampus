8622acfd0f4d0754b53ae17e4bdb930f2bdc35d3
Marco <mgomezp17@eafit.edu.co>
2024-11-28 00:45:25 -0500

    feat: add AnimationProvider component for automatic animations

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,9 @@
 import { Montserrat } from 'next/font/google';
-import { ViewTransitions } from 'next-view-transitions';
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from '@clerk/nextjs';
 
 import React from 'react';
 import '../../public/css/main.css';
+import AnimationProvider from '@/components/AnimationProvider';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -28,7 +22,18 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang='es' className={`${montserrat.className} overflow-hidden`}>
-        <body className='overflow-hidden bg-primary'>{children}</body>
+        <body className='overflow-hidden bg-primary'>
+          <AnimationProvider>
+            <ClerkLoading>
+              <div className='fixed top-0 left-0 z-50 w-full h-full bg-primary flex items-center justify-center'>
+                <div className='flex justify-center'>
+                  <span className='loading loading-infinity loading-lg bg-primary-orange'></span>
+                </div>
+              </div>
+            </ClerkLoading>
+            <ClerkLoaded>{children}</ClerkLoaded>
+          </AnimationProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
