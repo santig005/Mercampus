79fe8080636b1134c2ea8088f5f69290e6fae338
sajhdg30 <sajhdg30@gmail.com>
2025-01-20 18:38:04 +0000

    refactor: sellers folder distribution of project changed

--- src/app/antojos/layout.jsx
+++ src/app/antojos/layout.jsx
@@ -28,20 +28,26 @@ export default async function layout({ children }) {
               <SidebarBtn text='Antojitos' goto='/antojos/' />
             </li>
             <li>
-              <SidebarBtn text='Mira los vendedores' goto='/antojos/sellers' />
+              <SidebarBtn text='Mira los vendedores' goto='/antojos/sellers/list' />
             </li>
             {userId && (
               <>
                 <li>
                   <SidebarBtn
                     text='Quiero ser vendedor'
-                    goto='/antojos/registerseller'
+                    goto='/antojos/sellers/register'
+                  />
+                </li>
+                <li>
+                  <SidebarBtn
+                    text='Editar perfil de vendedor'
+                    goto='/antojos/sellers/profile/edit'
                   />
                 </li>
                 <li>
                   <SidebarBtn
                     text='Mis horarios'
-                    goto='/antojos/registerseller/schedules'
+                    goto='/antojos/sellers/schedules'
                   />
                 </li>
                 <li>
