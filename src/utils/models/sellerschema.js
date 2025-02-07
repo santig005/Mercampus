a075dc0358181804964a7ed264257d1cb3629b15
Marco <mgomezp17@eafit.edu.co>
2025-02-06 21:20:14 -0500

    style: improve formatting and readability of seller schema

--- src/utils/models/sellerschema.js
+++ src/utils/models/sellerschema.js
@@ -1,41 +1,36 @@
 import mongoose from 'mongoose';
 
 const sellerSchema = new mongoose.Schema({
-  businessName: {
-    type: String,
-    required: true,
-  },
-  slogan: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  logo: {
-    type: String,
-  },
-  instagramUser: {
-    type: String,
-  },
-  availability: {
-    type: Boolean,
-    default: true,
-  },
-  phoneNumber: {
-    type: Number,
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  approved: {
-    type: Boolean,
-    default: false,
-  },
+    businessName: {
+        type: String,
+        required: true
+    },
+    slogan: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    logo: {
+        type: String
+    },
+    instagramUser: {
+        type: String
+    },
+    availability: {
+        type: Boolean,
+        default: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
 });
 
-export const Seller =
-  mongoose.models.Seller || mongoose.model('Seller', sellerSchema);
+export const Seller = mongoose.models.Seller || mongoose.model('Seller', sellerSchema);
 export default Seller;
