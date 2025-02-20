4b3920deee331e275b32351513337187bd968708
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2025-02-19 19:05:48 -0500

    feat: implement seller approval and registration flow with redirection logic

--- src/app/antojos/sellers/approving/page.jsx
+++ src/app/antojos/sellers/approving/page.jsx
@@ -1,33 +1,32 @@
-import { currentUser } from '@clerk/nextjs/server';
-import { getUserByEmail } from '@/services/userService';
-import { redirect } from 'next/navigation';
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSeller } from "@/context/SellerContext";
 
-const SellerApprovalStatus = async () => {
-  const user = await currentUser();
-  // if (!user) {
-  //   redirect('/');
-  // }
-  const email = user.primaryEmailAddress.emailAddress;
+const SellerApprovalStatus = () => {
+  const router = useRouter();
+  const { seller, loading: sellerLoading } = useSeller();
 
-  const getUser = async email => {
-    const [userData] = await Promise.all([getUserByEmail(email)]);
-    if (!(userData.role === 'seller')) {
-      redirect('/antojos/sellers/register');
+  // Si ya está aprobado o si la carga del estado de vendedor ya ha terminado, redirigir.
+  useEffect(() => {
+    if (!sellerLoading) {
+      if (seller && seller.approved) {
+        router.push("/antojos/sellers/schedules");
+      }
     }
-  };
-  await getUser(email);
+  }, [seller, sellerLoading, router]);
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-[#F2F2F2] p-8'>
-      <div className='bg-[#FF7622] rounded-lg p-6 text-center'>
-        <h1 className='text-3xl font-bold mb-4'>
+    <div className="flex flex-col items-center justify-center h-screen bg-[#F2F2F2] p-8">
+      <div className="bg-[#FF7622] rounded-lg p-6 text-center">
+        <h1 className="text-3xl font-bold mb-4">
           Tu estado de vendedor está en proceso de aprobación
         </h1>
-        <p className='text-lg mb-4'>
+        <p className="text-lg mb-4">
           Por favor, espera mientras revisamos la información de tu negocio.
           Entra más tarde.
         </p>
-        <p className='text-sm text-gray-700'>
+        <p className="text-sm text-gray-700">
           Si tienes alguna pregunta, puedes contactarnos directamente.
         </p>
       </div>
