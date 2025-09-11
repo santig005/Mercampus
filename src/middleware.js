10f51f512247ea588381e118eadbcf3f455db842
sajhdg30 <sajhdg30@gmail.com>
2025-09-11 15:49:21 +0000

    feat: Redirect unauthenticated users to sign up page

--- src/middleware.js
+++ src/middleware.js
@@ -17,12 +17,12 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth();
+  const { userId, redirectToSignUp } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
     // Add custom logic to run before redirecting
 
-    return redirectToSignIn();
+    return redirectToSignUp();
   }
 }
 );
