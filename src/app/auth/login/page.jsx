3cdfedcb804f718aab5dbf54ded6fa9ea4bc6535
sajhdg30 <sajhdg30@gmail.com>
2025-09-16 17:14:19 +0000

    revert: Eliminar requisito de autenticación para botones de contacto
    Este commit revierte la implementación que requería que los usuarios iniciaran sesión para utilizar los botones de contacto (WhatsApp e Instagram) en las páginas de productos y vendedores.
    
    Se ha restaurado la funcionalidad anterior, permitiendo que todos los visitantes puedan contactar a los vendedores directamente sin necesidad de estar autenticados.

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,7 +1,6 @@
 import SignInForm from '@/components/auth/register/SignInForm';
 import React from 'react';
 
-export default function page({ searchParams }) {
-  const redirectTo = searchParams?.redirectTo || '/';
-  return <SignInForm redirectTo={redirectTo} />;
+export default function page() {
+  return <SignInForm />;
 }
